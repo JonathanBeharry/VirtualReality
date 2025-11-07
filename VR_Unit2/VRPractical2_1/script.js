3387b83bdecefb575156698073e5ee6345184683
JonathanBeharry <jonathanb161@nycstudents.net>
2025-11-07 13:28:40 +0000

    tree

--- VR_Unit2/VRPractical2_1/script.js
+++ VR_Unit2/VRPractical2_1/script.js
@@ -7,14 +7,26 @@ function rnd(l, u){
 window.addEventListener("DOMContentLoaded",function() {
   scene = document.querySelector("a-scene"); //CSS Selector
 
-  for(let i = 0; i < 100; i++){
+  for(let i = 0; i < 30; i++){
     let x = rnd(-20,20);
     let z = rnd(-20,20);
     createTree(x,0,z);
   }
    //Task 2: Use the createCloud(...)  to add several clouds to the scene at various positions.
-
+  for(let i = 0; i < 50; i++){
+    let x = rnd(-20,20);
+    let z = rnd(-20,20);
+    createCloud(x,0,z);
+  }
    //Task 4: Use the createHouse(...)  to add several houses to the scene at various positions.
+  for(let i = 0; i < 25; i++){
+    let x = rnd(-20,20);
+    let z = rnd(-20,20);
+    createHouse(x,0,z);
+  }
+
+
+
 })
 
 /* Task 1: Create a function createCloud that,
@@ -26,6 +38,38 @@ window.addEventListener("DOMContentLoaded",function() {
       6) Add the cloud entity to the scene
 */
 
+function createCloud(x, y, z){
+  let cloud = document.createElement("a-entity");
+
+  // puff one
+  let puff = document.createElement("a-sphere");
+  puff.setAttribute("color", "white");
+  puff.setAttribute("radius", "3");
+  puff.setAttribute("position", "0 10 0");
+  cloud.append(puff);
+
+  // puff 2
+  let puff2 = document.createElement("a-sphere");
+  puff2.setAttribute("color", "white");
+  puff2.setAttribute("radius", "2");
+  puff2.setAttribute("position", "0.5 10 0");
+  
+  cloud.append(puff2);
+
+  // puff 3
+  let puff3 = document.createElement("a-sphere");
+  puff3.setAttribute("color", "white");
+  puff3.setAttribute("radius", "2");
+  puff3.setAttribute("position", "-0.5 10 0");
+
+  cloud.append(puff3);
+  
+  
+  cloud.setAttribute("position", {x:x , y:y , z:z});
+
+  scene.append(cloud);
+}
+
 /* Task 3: Create a function createHouse that,
       1) Accept an x and z position for where to place the house "entity"
       2) Create an entity to store all the components that will make up the house
@@ -35,6 +79,31 @@ window.addEventListener("DOMContentLoaded",function() {
       5) Set house entities position to those passed in to the function.
       6) Add the house entity to the scene
 */
+
+function createHouse(x, y, z){
+  let house = document.createElement("a-entity");
+  
+  let wall1 = document.createElement("a-box");
+    wall1.setAttribute("color","white");
+    wall1.setAttribute("position","-2 1.5 0");
+    wall1.setAttribute("height","3");
+    wall1.setAttribute("width","4");
+    house.append( wall1 );
+
+    let roof = document.createElement("a-cone");
+    roof.setAttribute("color","brown");
+    roof.setAttribute("position","-2 4 0");
+    roof.setAttribute("height","2");
+    roof.setAttribute("radius-bottom","3");
+    house.append( roof ); 
+  
+    house.setAttribute("position",{x:x, y:y, z:z});
+    scene.append(house)
+}
+
+
+
+
 function createTree(x, y, z){
   let tree = document.createElement("a-entity");
   
@@ -53,6 +122,3 @@ function createTree(x, y, z){
   tree.setAttribute("position",{x:x, y:y, z:z});
   scene.append( tree )
 }
-
-
-
--- VR_Unit2/VRPractical2_4/script.js
+++ VR_Unit2/VRPractical2_4/script.js
@@ -1,15 +1,37 @@
 let rnd = (l,u) => Math.floor(Math.random()*(u-l) + l);
 let scene;
+let car;
 
-window.addEventListener("DOMContentLoaded",function() {
-  scene = document.querySelector("a-scene"); 
-
-
+window.addEventListener("DOMContentLoaded",function() {let scene;
+let rocket;
+scene = document.querySelector("a-scene"); //CSS Selector
+  rocket = document.querySelector("#rocket");
+  rocket.a = 0;
+  rocket.da = 0.01;
   loop();
-})
 
-function loop(){
+function loop (){
+  rocket.a += rocket.da;
+  rocket.setAttribute("position", {x:3 , y:rocket.a , z:-4});
+  window.requestAnimationFrame(loop);
+ 
+  car = document.querySelector("#car");
+  car.a = 0;
+  car.da = -0.01;
 
-  
-  window.requestAnimationFrame( loop );
+  // car loop
+function loop (){
+  car.a += car.da;
+  car.setAttribute("position", {x:car.a , y:0 , z:-8});
+  window.requestAnimationFrame(loop4);
 }
+}}// car loop
+function loop (){
+  car.a += car.da;
+  car.setAttribute("position", {x:car.a , y:0 , z:-8});
+  window.requestAnimationFrame(loop);
+for(let i = 0; i < 20; i++){
+    let rocket = new rocket(rnd(-20,20), 0 , rnd(-20,20));
+    rocket.scale(rnd(1,4));
+}
+
